5c4f28fa9b1febe2d89f346d0877c0a0ea6262e5
StarkOdinson612 <darkknightishaan@gmail.com>
2021-05-31 20:21:16 +0530

    added time and feed

--- sketch.js
+++ sketch.js
@@ -5,6 +5,7 @@ var tInt = 2500;
 let feedButton, stockButton;
 let fedTime, lastFed;
 let foodObj;
+let stockR, stock;
 
 function preload()
 {
@@ -21,13 +22,6 @@ function setup() {
 
   foodObj = new Food();
 
-  feedButton = createButton('Feed Dog');
-  feedButton.position(350,50);
-
-  stockButton = createButton('Add Food');
-  stockButton.position(520,50);
-  stockButton.addClass('stock')
-
   dogS = createSprite(500, 400, 350, 350);
   dogS.addImage("regular", dog);
   dogS.scale = 0.5;
@@ -39,6 +33,55 @@ function setup() {
   fedTime.on('value', function(data) {
     lastFed = data.val()
   })
+
+  stockR = database.ref('Food')
+  stockR.on('value', function(data) {
+    stock = data.val();
+  })
+
+  feedButton = createButton('Feed Dog');
+  feedButton.position(350,50);
+  feedButton.mousePressed(() => {
+    a = stock;
+    b = foodObj.foodStock;
+    
+    d = new Date();
+    year = d.getFullYear();
+    month = d.getMonth(); + 1;
+    date = d.getDate();
+    hour = (d.getHours() + 1);
+
+    if (hour > 12) {
+      tId = 'pm';
+      finHour = hour -= 12;
+    }
+    else {
+      tId = 'am';
+      finHour = hour;
+    }
+
+    minute = d.getMinutes() + 1;
+    second = d.getSeconds() + 1;
+
+    finDate = `${date}/${month}/${year}  ${finHour}:${minute}:${second} ${tId}`;
+
+    database.ref('/').update({
+      Food: a + 1,
+      FoodStock: b - 1,
+      LastFed: finDate
+    });
+  });
+
+  stockButton = createButton('Add Food');
+  stockButton.position(520,50);
+  stockButton.addClass('stock')
+  stockButton.mousePressed(() => {
+    b = foodObj.foodStock;
+
+    database.ref('/').update({
+      FoodStock : b + 1
+    });
+  })
 }
 
 
@@ -46,12 +89,12 @@ function draw() {
   background(46, 139, 87);
   foodObj.display();
   tInt = randomInteger(55, 70) * 100;
-  console.log(foodS)
+  console.log(foodObj.foodStock)
 
   textSize(30);
   fill(255);
   
-  text(foodS, 100, 100);
+  text(stock, 100, 100);
   text(lastFed, 100, 150);
 
   drawSprites();
@@ -61,7 +104,7 @@ function readStock(data) {
   foodObj.foodStock = data.val()
 }
 
-function writeStock(x, y=1) {
+function writeStock(x, y=-1) {
 
   if (x <= 0) {
     x = 0;
@@ -86,4 +129,9 @@ function randomInteger(min, max) {
 //    writeStock(foodS, f)
    
 // }, tInt)
+
+setInterval(function() {
+  f = -1;
+  writeStock(stock, f);
+}, tInt)
 
